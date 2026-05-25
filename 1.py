5fd21d7dacb73e9d037039413b2e5878320055a1
Nopene2007 <stupitdogz@gmail.com>
2026-05-25 21:23:31 +0700

    Add 1.py via Git Upload 27NetTeam

---
+++ 1.py
@@ -0,0 +1,13 @@
+# phan tich loi
+# sua code
+student_name = "  nguYEn vAn a  "
+student_code = "  rk-001-python  "
+email = "  Student01@GMAIL.COM  "
+
+riel_name=student_name.strip().title()
+riel_code=student_code.strip().upper()
+riel_email=email.strip().lower()
+
+print("Họ tên:", riel_name)
+print("Mã học viên:", riel_code)
+print("Email:",riel_email )
